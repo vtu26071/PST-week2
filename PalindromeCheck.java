931ea1931ef85b092f47e4e890a742908d2811ad
vtu26071 <vtu26071@veltech.edu.in>
2026-04-19 09:19:23 +0530

    Add PalindromeCheck program to check for palindromes

---
+++ PalindromeCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        if (str.equalsIgnoreCase(rev)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+        sc.close();
+    }
+}
