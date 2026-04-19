8a713708ed46fe26a662b285851dd27a72fc39f9
vtu26071 <vtu26071@veltech.edu.in>
2026-04-19 09:20:02 +0530

    Add AllDigitsCount program to count digits
    Implement a program to count all digits in a string input.

---
+++ AllDigitsCount.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class AllDigitsCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
